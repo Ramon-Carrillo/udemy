4f4f4dd7ad2595d2c13422b8ac13270f78fbacc2
Ramon-Carrillo <carrillojrramon@gmail.com>
2022-08-18 23:54:13 -0500

    coded project using json server and useEffect for fetching data

---
+++ Build Web Apps with React & Firebase/tripago/src/components/Loading.js
@@ -0,0 +1,11 @@
+import './LoadingSpinner.css'
+
+const Loading = () => {
+  return (
+    <div className='center'>
+      <div className='ring'></div>
+      <span>loading...</span>
+    </div>
+  )
+}
+export default Loading
