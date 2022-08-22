95c8a385073ca2546debe0c4e3d7d02a9e38b6b0
Ramon-Carrillo <carrillojrramon@gmail.com>
2022-08-22 17:40:18 -0500

    finished magic match project

---
+++ Build Web Apps with React & Firebase/magic-memory/src/components/SingleCard.js
@@ -0,0 +1,24 @@
+import './SingleCard.css'
+
+const SingleCard = ({ card, handleChoice, flipped, disabled }) => {
+  const handleClick = () => {
+    if (!disabled) {
+      handleChoice(card)
+    }
+  }
+
+  return (
+    <div className='card'>
+      <div className={flipped ? 'flipped' : ''}>
+        <img src={card.src} className='front' alt={card.front} />
+        <img
+          src='/img/cover.png'
+          className='back'
+          alt='cover'
+          onClick={handleClick}
+        />
+      </div>
+    </div>
+  )
+}
+export default SingleCard
